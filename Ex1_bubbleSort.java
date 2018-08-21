de3866c0e576f23629937c1c84d0ea68ae603d38
BrenoWanderson <42473917+BrenoWanderson@users.noreply.github.com>
2018-08-21 17:54:41 -0300

    Add files via upload

---
+++ Ex1_bubbleSort.java
@@ -0,0 +1,29 @@
+
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class Ex1_bubbleSort {
+    public static void main(String[] args) {
+        Scanner tcl = new Scanner(System.in);
+        Random numeros = new Random ();
+        int aux = 0;
+        int vetor[] = new int[10];
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = numeros.nextInt();
+        }
+        for (int i = 0; i < vetor.length; i++) {
+            for (int j = 0; j < vetor.length; j++) {
+                if (vetor[i] < vetor[j]) {
+                    aux = vetor[i];
+                    vetor[i] = vetor[j];
+                    vetor[j] = aux;
+                }
+            }
+        }
+        System.out.println("numeros ordenados");
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println(vetor[i]);
+        }
+    }
+}
